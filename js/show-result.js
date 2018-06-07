58e29e9550a5f918c818360cbb124b8a8eec4986
Katya Leontyeva <k.leontyeva.mail@gmail.com>
2018-06-07 23:28:47 +0300

    Список результатов сортируется по убыванию количества набранных очков

--- js/show-result.js
+++ js/show-result.js
@@ -7,9 +7,9 @@ export const showResult = (scores, gameResult) => {
     return `У вас закончились все попытки. Ничего, повезёт в следующий раз!`;
   }
 
-  const newScores = scores.concat(gameResult.points).sort((a, b) => a - b);
+  const newScores = scores.concat(gameResult.points).sort((a, b) => b - a);
   const players = newScores.length;
-  const place = players - newScores.indexOf(gameResult.points);
+  const place = newScores.indexOf(gameResult.points) + 1;
   const rate = Math.round(((players - place) / players) * 100);
 
   return `Вы заняли ${place} место из ${players} игроков. Это лучше, чем у ${rate}% игроков`;

cc3b87e0bd69ee1c48a122401aa134a25ecbb115
Katya Leontyeva <k.leontyeva.mail@gmail.com>
2018-06-10 00:49:12 +0300

    Добавлена функция создания таймера и тест

--- js/calculate-points.js
+++ js/calculate-points.js
@@ -1,5 +1,5 @@
 const TOTAL_QUESTIONS = 10;
-const FAST_ANSWER_TIME = 30; // 30 sec
+const FAST_ANSWER_TIME = 30000; // 30 sec
 const LOSE_POINTS = -1;
 
 export const calculatePoints = (answers, attempts) => {
@@ -13,7 +13,7 @@ export const calculatePoints = (answers, attempts) => {
     if (attemptsLeft > 0) {
       if (answer.correct) {
         points++;
-        if (answer.time < FAST_ANSWER_TIME) {
+        if (answer.time * 1000 < FAST_ANSWER_TIME) {
           points++;
         }
       } else {
---
+++ js/timer.js
@@ -0,0 +1,21 @@
+export const createTimer = (seconds) => {
+  if (!Number.isInteger(seconds)) {
+    throw new Error(`Time should not be non integer value`);
+  }
+
+  if (seconds < 0) {
+    throw new Error(`Time should not be negative value`);
+  }
+
+  return {
+    timeLeft: seconds,
+    tick() {
+      if (this.secondsLeft > 0) {
+        this.secondsLeft--;
+        return this.timeLeft;
+      } else {
+        return `Время вышло!`;
+      }
+    }
+  };
+};

d30057e4e81036a8d10c23b20d59a73557543034
Ekaterina Leonteva <k.leontyeva.mail@gmail.com>
2018-06-15 19:14:33 +0300

    Добавлено отображение вариантов ответа на экране игры

--- js/game/game-screen.js
+++ js/game/game-screen.js
@@ -1,5 +1,5 @@
 import {changeScreen, render} from '../util';
-import {initialState} from "../data/game-data";
+import {initialState, gameQuestions} from "../data/game-data";
 import gameHeader from "./game-header";
 
 const gameScreen = (question) => {
@@ -17,7 +17,15 @@ const gameScreen = (question) => {
           </div>
         </div>
         <form class="main-list">
-        Ответы
+          ${[...Object.entries(question.answers)].map(([answerId, answerData], index) =>
+          `<div class="main-answer-wrapper">
+            <input class="main-answer-r" type="radio" id="${answerId}" name="answer" value="val-${index + 1}"/>
+            <label class="main-answer" for="${answerId}">
+              <img class="main-answer-preview" src="${answerData.track.image}"
+                   alt="${answerData.track.name}" width="134" height="134">
+              ${answerData.track.name}
+            </label>
+          </div>`).join(``)}
         </form>
       </div>
     </section>
@@ -28,7 +36,21 @@ const gameScreen = (question) => {
       <div class="main-wrap">
         <h2 class="title">${question.title}</h2>
         <form class="genre">
-          Ответы
+          ${[...Object.entries(question.answers)].map(([answerId, answerData], index) =>
+          `<div class="genre-answer">
+            <div class="player-wrapper">
+              <div class="player">
+                <audio src="${answerData.track.src}"></audio>
+                <button class="player-control player-control--play"></button>
+                <div class="player-track">
+                  <span class="player-status"></span>
+                </div>
+              </div>
+            </div>
+            <input type="checkbox" name="answer" value="answer-1" id="a-${index + 1}">
+            <label class="genre-answer-check" for="a-${index + 1}"></label>
+          </div>`).join(``)}
+
           <button class="genre-answer-send" type="submit">Ответить</button>
         </form>
       </div>
@@ -42,6 +64,37 @@ const gameScreen = (question) => {
 
   const gameScreenElement = render(questionTemplateMap[question.type]);
   gameScreenElement.insertAdjacentElement(`afterbegin`, gameHeader(initialState));
+
+  const onAnswerSend = () => {
+    changeScreen(gameScreen(gameQuestions[6]));
+  };
+
+  if (question.type == `guessArtist`) {
+    const artistForm = gameScreenElement.querySelector(`form.main-list`);
+    const artistAnswers = Array.from(artistForm.querySelectorAll(`.main-answer`));
+    artistAnswers.forEach((answer) => {
+      // Записывать ответ и время в gameState
+      answer.addEventListener(`click`, onAnswerSend);
+    });
+  } else if (question.type == `chooseGenre`) {
+    const genreForm = gameScreenElement.querySelector(`form.genre`);
+    const genreAnswers = Array.from(genreForm.querySelectorAll(`input[name="answer"]`));
+    const sendAnswerButton = genreForm.querySelector(`.genre-answer-send`);
+
+    const onAnswerChange = () => {
+      const someAnswersChecked = genreAnswers.some((el) => el.checked === true);
+      sendAnswerButton.disabled = !someAnswersChecked;
+    };
+
+    // Кнопка «Ответить» отключена, пока не выбран ни один из вариантов ответа.
+    sendAnswerButton.disabled = true;
+
+    genreAnswers.forEach((answer) => {
+      answer.addEventListener(`change`, onAnswerChange);
+    });
+
+    sendAnswerButton.addEventListener(`click`, onAnswerSend);
+  }
 
   return gameScreenElement;
 };

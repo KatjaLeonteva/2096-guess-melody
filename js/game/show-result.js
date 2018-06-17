43a2afe4e00a2e9f976e84a15c78a14e967bbccf
Katya Leontyeva <k.leontyeva.mail@gmail.com>
2018-06-17 18:19:32 +0300

    Рефакторинг модуля показа результатов игры

--- js/game/show-result.js
+++ js/game/show-result.js
@@ -3,36 +3,55 @@ import {GAME_SETTINGS} from "../data/game";
 import {statistics} from "../data/game-data";
 import {calculatePoints} from "./calculate-points";
 
-export const showResult = (gameState) => {
-  if (gameState.timeLeft === 0) {
-    return {
+const getResult = (state) => {
+  if (state.timeLeft === 0) {
+    return `timeup`;
+  }
+
+  if (state.mistakes === GAME_SETTINGS.maxMistakes) {
+    return `lose`;
+  }
+
+  return `win`;
+};
+
+const resultTemplate = (game) => {
+  return {
+    timeup: {
       title: `Увы и ах!`,
       description: `Время вышло!<br>Вы не успели отгадать все мелодии`,
       comparison: `` // TODO убрать после исправления resultScreenTemplate
-    };
-  }
-
-  if (gameState.mistakes === GAME_SETTINGS.maxMistakes) {
-    return {
+    },
+    lose: {
       title: `Какая жалость!`,
       description: `У вас закончились все попытки.<br>Ничего, повезёт в следующий раз!`,
       comparison: `` // TODO убрать после исправления resultScreenTemplate
-    };
-  }
-
-  const userPoints = calculatePoints(gameState);
-  const gameTime = msToMinutesAndSeconds(GAME_SETTINGS.totalTime - gameState.timeLeft);
-  const fastAnswers = gameState.answers.filter((answer) => answer.correct && answer.time < GAME_SETTINGS.fastAnswerTime).length;
-  const updatedStatistics = statistics.concat(userPoints).sort((a, b) => b - a);
-  const players = updatedStatistics.length;
-  const place = updatedStatistics.indexOf(userPoints) + 1;
-  const rate = Math.round(((players - place) / players) * 100);
-
-  return {
-    title: `Вы настоящий меломан!`,
-    description: `За&nbsp;${gameTime.minutes}&nbsp;${pluralize(gameTime.minutes, `minutes`)} и ${gameTime.seconds}&nbsp;${pluralize(gameTime.seconds, `seconds`)}
-      <br>вы&nbsp;набрали ${userPoints} ${pluralize(userPoints, `points`)} (${fastAnswers} ${pluralize(fastAnswers, `fast`)})
-      <br>совершив ${gameState.mistakes}&nbsp;${pluralize(gameState.mistakes, `mistakes`)}`,
-    comparison: `Вы заняли ${place} место из ${players}. Это&nbsp;лучше, чем у&nbsp;${rate}%&nbsp;игроков`
+    },
+    win: {
+      title: `Вы настоящий меломан!`,
+      description: `За&nbsp;${game.time.minutes}&nbsp;${pluralize(game.time.minutes, `minutes`)} и ${game.time.seconds}&nbsp;${pluralize(game.time.seconds, `seconds`)}
+      <br>вы&nbsp;набрали ${game.points} ${pluralize(game.points, `points`)} (${game.fastAnswers} ${pluralize(game.fastAnswers, `fast`)})
+      <br>совершив ${game.mistakes}&nbsp;${pluralize(game.mistakes, `mistakes`)}`,
+      comparison: `Вы заняли ${game.place} место из ${game.players}. Это&nbsp;лучше, чем у&nbsp;${game.rate}%&nbsp;игроков`
+    }
   };
+};
+
+export const showResult = (gameState) => {
+  const userPoints = calculatePoints(gameState);
+  const updatedStatistics = statistics.concat(userPoints).sort((a, b) => b - a);
+
+  let gameResult = {
+    time: msToMinutesAndSeconds(GAME_SETTINGS.totalTime - gameState.timeLeft),
+    points: userPoints,
+    fastAnswers: gameState.answers.filter((answer) => answer.correct && answer.time < GAME_SETTINGS.fastAnswerTime).length,
+    mistakes: gameState.mistakes,
+    players: updatedStatistics.length,
+    place: updatedStatistics.indexOf(userPoints) + 1,
+    get rate() {
+      return Math.round(((this.players - this.place) / this.players) * 100);
+    }
+  };
+
+  return resultTemplate(gameResult)[getResult(gameState)];
 };
